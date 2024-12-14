46072ca82dd57f964326820beba18aef725781a7
enockuwumukiza <wwwenockuwumukiza@gmail.com>
2024-12-14 04:02:30 +0200

    adding env

--- banking_system/settings.py
+++ banking_system/settings.py
@@ -12,6 +12,16 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Access the ALLOWED_HOSTS environment variable
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +36,6 @@ SECRET_KEY = 'django-insecure-&-+qmpupaq29dv9b#+3a74w7zc52(tx%hz^c!$2qhma8rxq1&t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://banking-system-j0z4.onrender.com']
 
 
 # Application definition
