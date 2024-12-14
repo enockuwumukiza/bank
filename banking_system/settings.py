11c1c090f7a3f94ea396066baa48fbfd77e094fc
enockuwumukiza <wwwenockuwumukiza@gmail.com>
2024-12-14 03:48:54 +0200

    adding settings

--- banking_system/settings.py
+++ banking_system/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&-+qmpupaq29dv9b#+3a74w7zc52(tx%hz^c!$2qhma8rxq1&t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://banking-system-j0z4.onrender.com']
 
