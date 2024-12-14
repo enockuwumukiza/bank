fb816220ba4ddf1f142db13077afd75ceea38358
enockuwumukiza <wwwenockuwumukiza@gmail.com>
2024-12-14 06:33:45 +0200

    cleanup

--- banking_system/settings.py
+++ banking_system/settings.py
@@ -96,7 +96,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         "DIRS": [
-            BASE_DIR / 'templates', 
         ],
         'APP_DIRS': True,
         'OPTIONS': {
